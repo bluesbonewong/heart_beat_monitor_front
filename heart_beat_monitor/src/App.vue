067308d7b6969cde3f16782f55aea994d10773ff
wangruilin <bluesbonewong@gmail.com>
2020-12-13 16:08:21 +0800

    App.vue里的keep-alive配置和css设置

--- heart_beat_monitor/src/App.vue
+++ heart_beat_monitor/src/App.vue
@@ -1,32 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- router-view显示的是当前路由地址所对应的内容，所有的内容都会通过router-view进行展示 -->
+    <keep-alive>
+      <router-view />
+    </keep-alive>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background: #fafbfc;
 }
 </style>
