7293cb13dfb09ac875c9738228ebeb9889e56837
wangruilin <bluesbonewong@gmail.com>
2020-12-14 20:54:58 +0800

    delete About.vue

--- heart_beat_monitor/src/router/index.js
+++ heart_beat_monitor/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '@/store/index'
-import Home from '@/views/Home.vue'
+import Home from '@/views/Home/Home.vue'
 import Login from '@/views/Login'
 
 Vue.use(VueRouter)
@@ -16,14 +16,6 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: Login
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     },
     {
         path: '*', // 此处需特别注意置于最底部
