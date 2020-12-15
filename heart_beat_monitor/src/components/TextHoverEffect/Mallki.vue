69ce5fa3b94f9088b97600bdc3221816bb037f81
wangruilin <bluesbonewong@gmail.com>
2020-12-15 23:11:55 +0800

    更新Mallki的颜色

--- heart_beat_monitor/src/components/TextHoverEffect/Mallki.vue
+++ heart_beat_monitor/src/components/TextHoverEffect/Mallki.vue
@@ -26,7 +26,7 @@ export default {
 
 .link--mallki {
   font-weight: 800;
-  color: #4dd9d5;
+  color: #a34dd9;
   font-family: 'Dosis', sans-serif;
   -webkit-transition: color 0.5s 0.25s;
   transition: color 0.5s 0.25s;
