8b800840f830a87ad423b7fed40fe46a0aac079e
wangruilin <bluesbonewong@gmail.com>
2020-12-17 21:50:06 +0800

    历史结点状态chart completed

--- heart_beat_monitor/src/views/ClientNodes/historyStatusChart.vue
+++ heart_beat_monitor/src/views/ClientNodes/historyStatusChart.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="history-status-chart">
-    historyStatusChart
+    <chart height="100%" width="100%" />
   </div>
 </template>
 
 <script>
+import Chart from '@/views/ClientNodes/components/charts/MixChartHistory.vue'
+
 export default {
-  name: 'historyStatusChart'
+  name: 'historyStatusChart',
+  components: {Chart}
 }
 </script>
 
 <style scoped>
-
+.history-status-chart {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 84px);
+}
 </style>
